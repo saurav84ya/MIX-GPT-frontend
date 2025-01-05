b68a9b192eee0f8494ed0c405eb86fef6d1a9dd5
saurav84ya <sauravkumarobra50@gmail.com>
2025-01-05 20:11:34 +0530

    auth backend frontend completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
     <div>
-      Home
+      <div class="flex relative  ">
+        <div class="  w-[300px] xl:relative absolute bg-red-500">Fixed Width Item</div>
+
+        <div class="flex-1  bg-blue-500">Flexible Item</div>
+      </div>
     </div>
-  )
+  );
 }
